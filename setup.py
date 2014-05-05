f4a6fb9fff8ebd55d64ec22fd2c06e55421450f8
touwwouter <woutertouw@hotmail.com>
2014-05-05 14:38:21 +0200

    Bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='bdb',
-    version='0.5.3.5',
+    version='0.6.1',
     description='A databank of PDB entries with full isotropic B-factors.',
     author='Wouter Touw',
     author_email='wouter.touw@radboudumc.nl',
