76be017de5ea91a55ba6b0d80eebb774fc45fecf
touwwouter <woutertouw@hotmail.com>
2014-05-12 15:07:10 +0200

    Add license attributes to setup and readme

--- setup.py
+++ setup.py
@@ -7,11 +7,13 @@ setup(
     description='A databank of PDB entries with full isotropic B-factors.',
     author='Wouter Touw',
     author_email='wouter.touw@radboudumc.nl',
+    license = "GNU GPLv3",
     url='https://github.com/cmbi/bdb',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Scientific/Engineering :: Bio-Informatics',
